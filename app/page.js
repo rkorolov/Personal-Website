e29ab45777bd6570d5da545235be34451788a98c
Ruslana Korolov <rkorolov@icloud.com>
2023-12-06 21:57:24 -0800

    Navbar component made

--- app/page.js
+++ app/page.js
@@ -1,3 +1,25 @@
 export default function Home() {
-  return <p>Hello World!</p>
+  return (
+    <Navbar />
+  );
+}
+
+function Navbar() {
+  return (
+    <nav className="border-b-4 sticky top-0 bg-lime-600 text-amber-100 border-lime-600 z-10">
+      <div className="h-14 max-w-7xl p-4 mx-auto flex items-center justify-between">
+        <a href="/" className="font-medium text-lg md:hover:underline">
+          My Website
+        </a>
+        <ul className="hidden md:flex items-center justify-end space-x-4 text-sm font-medium">
+          <li className="md:hover:underline">
+            <a href="/blog">Blog</a>
+          </li>
+          <li className="md:hover:underline">
+            <a href="/photos">Photos</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
