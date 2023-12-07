22704ab78b13a35038898dba14acd6a2e5619865
Ruslana Korolov <rkorolov@icloud.com>
2023-12-07 00:05:50 -0800

    More Components Added

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -2,7 +2,9 @@ export default function Footer() {
     return (
       <nav className="bg-secondary-200 rounded-lg shdow m-4 dark:bg-amber-400 place-content-end absolute inset-x-0 bottom-0">
         <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-        <span class="text-sm text-pink-400 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.
+        
+        <span>
+        <p class="text-pink-400">Ruslana K &copy; {new Date().getFullYear()}</p>
         </span>
           <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-pink-400 dark:text-gray-400 sm:mt-0">
             <ul class="list-image-[git.png]">
