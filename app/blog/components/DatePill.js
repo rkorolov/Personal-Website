92e07c618f48d6902b5d238f146b71b3ef74f82d
Ruslana Korolov <114115390+rkorolov@users.noreply.github.com>
2026-01-30 16:12:07 -0800

    removing sanity imports

--- app/blog/components/DatePill.js
+++
@@ -1,9 +0,0 @@
-import { format } from "date-fns";
-
-export default function DatePill({ date }) {
-  return (
-    <p className="text-xs font-medium px-2 py-1 rounded-full bg-sage text-cream inline">
-      {format(new Date(date), "MMMM yyyy")}
-    </p>
-  );
-}
